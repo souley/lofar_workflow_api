d132444d44f80094f5613a5678876e996a36966d
bldevries-escience <b.devries@esciencecenter.nl>
2018-06-25 09:46:06 +0200

    Made some changes in the notebooks and models

--- lofar_workflow_api/api/models.py
+++ lofar_workflow_api/api/models.py
@@ -2,43 +2,47 @@ from django.db import models
 
 # Create your models here.
 class Session(models.Model):
-	name = models.CharField(max_length=255, blank=False, unique=False)
-	# observation = models.ForeignKey('Observation', 
-	# 	related_name = 'sessions', 
-	# 	on_delete=models.CASCADE)
-	# 	#models.IntegerField(default=0)
-	# pipeline_conf = models.ForeignKey('PipelineConfiguration', 
-	# 	related_name = 'sessions', 
-	# 	on_delete=models.CASCADE)
-	# owner = models.ForeignKey('auth.User',
-	# 	related_name = 'sessions', 
-	# 	on_delete = models.CASCADE)
-	date_created = models.DateTimeField(auto_now_add=True)
-	date_modified = models.DateTimeField(auto_now=True)
+    # API specific properties: 
+    name = models.CharField(max_length=255, blank=False, unique=False)
+    status = models.CharField(max_length = 20, choices=(("unknown", "unknown"), ("running", "running"), ("finished", "finished")), default = "unknown")
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
 
-	def __str__(self):
-		return "{}".format(self.name)
+    # Pipeline configuration properties:
+    avg_freq_step = models.IntegerField(default=2) # minimum=0. maximum=1000 "AVG_TIME_STEP"
+    avg_time_step = models.IntegerField(default=4) # minimum=0. maximum=1000 "AVG_TIME_STEP"
+    do_demix = models.BooleanField(default=True) # "DO_DEMIX"
+    demix_freq_step = models.IntegerField(default=2) #minimum=0. maximum=1000 "DEMIX_FREQ_STEP"
+    demix_time_step = models.IntegerField(default=2) # "DEMIX_TIME_STEP"
+    demix_sources = models.CharField(max_length=4, choices=(("CasA", "CasA"), ("CygA","CygA")), default="CasA") # "DEMIX_SOURCES"
+    select_NL = models.BooleanField(default=True) #"SELECT_NL"
+    parset = models.CharField(max_length=7, choices=\
+        (("", ""), ("hba_npp", "hba_npp"), ("hba_raw", "hba_raw"), ("lba_npp", "lba_npp"), ("lba_raw", "lba_raw"))\
+        , default = "lba_npp") # "PARSET"
+
+    def __str__(self):
+        return "{}".format(self.name)
 
 # class PipelineConfiguration(models.Model):
-# 	# owner = models.ForeignKey('auth.User', related_name = 'pipelineconfigurations', on_delete= models.CASCADE)
-# 	date_created = models.DateTimeField(auto_now_add=True)
-# 	date_modified = models.DateTimeField(auto_now=True)
+#   # owner = models.ForeignKey('auth.User', related_name = 'pipelineconfigurations', on_delete= models.CASCADE)
+#   date_created = models.DateTimeField(auto_now_add=True)
+#   date_modified = models.DateTimeField(auto_now=True)
 
-# 	name = models.CharField(max_length=255, blank=False, unique=False)
-# 	some_setting_1 = models.FloatField(default=0)
-# 	some_setting_2 = models.FloatField(default=0)
+#   name = models.CharField(max_length=255, blank=False, unique=False)
+#   some_setting_1 = models.FloatField(default=0)
+#   some_setting_2 = models.FloatField(default=0)
 
-# 	def __str__(self):
-# 		return "{}".format(self.name)
+#   def __str__(self):
+#       return "{}".format(self.name)
 
 
 # class Observation(models.Model):
-# 	# owner = models.ForeignKey('auth.User', related_name = 'observation', on_delete= models.CASCADE)
-# 	date_created = models.DateTimeField(auto_now_add=True)
-# 	date_modified = models.DateTimeField(auto_now=True)
+#   # owner = models.ForeignKey('auth.User', related_name = 'observation', on_delete= models.CASCADE)
+#   date_created = models.DateTimeField(auto_now_add=True)
+#   date_modified = models.DateTimeField(auto_now=True)
 
-# 	name = models.CharField(max_length=255, blank=False, unique=False)
-# 	some_identifier = models.CharField(max_length=255, blank=False, unique=False)
+#   name = models.CharField(max_length=255, blank=False, unique=False)
+#   some_identifier = models.CharField(max_length=255, blank=False, unique=False)
 
-# 	def __str__(self):
-# 		return "{}".format(self.name)
+#   def __str__(self):
+#       return "{}".format(self.name)
--- lofar_workflow_api/api/serializers.py
+++ lofar_workflow_api/api/serializers.py
@@ -2,31 +2,35 @@ from rest_framework import serializers
 from .models import *#Session, PipelineConfiguration
 
 class SessionSerializer(serializers.ModelSerializer):
-	""" Serializer to map the Model instance into JSON format."""
+    """ Serializer to map the Model instance into JSON format."""
 
-	#owner = serializers.ReadOnlyField(source = 'owner.username')
+    #owner = serializers.ReadOnlyField(source = 'owner.username')
 
-	class Meta:
-		"""Meta class to map serializer's fields with model fields"""
-		model = Session
-		fields = ('id', 'name', 'date_created', 'date_modified') #'owner', 'pipeline_conf', 'observation', 
-		read_only_field = ('date_created', 'date_modified')
+    class Meta:
+        """Meta class to map serializer's fields with model fields"""
+        model = Session
+        fields = (\
+            'id', 'name', 'date_created', 'date_modified',\
+            "avg_freq_step", "avg_time_step", "do_demix", "demix_freq_step", \
+            "demix_time_step", "demix_sources", "select_NL", "parset", \
+            )
+        read_only_field = ('date_created', 'date_modified', 'status')
 
 # class PipelineConfigurationSerializer(serializers.ModelSerializer):
 
-# 	#owner = serializers.ReadOnlyField(source = 'owner.username')
+#   #owner = serializers.ReadOnlyField(source = 'owner.username')
 
-# 	class Meta:
-# 		model = PipelineConfiguration
-# 		fields = ('id', 'name', 'some_setting_1', 'some_setting_2') #'owner', 
-# 		read_only_field = ('date_created', 'date_modified')
+#   class Meta:
+#       model = PipelineConfiguration
+#       fields = ('id', 'name', 'some_setting_1', 'some_setting_2') #'owner', 
+#       read_only_field = ('date_created', 'date_modified')
 
 
 # class ObservationSerializer(serializers.ModelSerializer):
 
-# 	#owner = serializers.ReadOnlyField(source = 'owner.username')
+#   #owner = serializers.ReadOnlyField(source = 'owner.username')
 
-# 	class Meta:
-# 		model = Observation
-# 		fields = ('id', 'name', 'some_identifier') # 'owner', 
-# 		read_only_field = ('date_created', 'date_modified')
+#   class Meta:
+#       model = Observation
+#       fields = ('id', 'name', 'some_identifier') # 'owner', 
+#       read_only_field = ('date_created', 'date_modified')
